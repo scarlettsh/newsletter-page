4b990cf9c74ac9924e02d0bae4a82f30e07893da
sijiahu <scarlettshcode@gmail.com>
2022-07-11 17:59:01 -0500

    update files to heroku git repo

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const request = require("request");
 const https = require("node:https");
-const { parse } = require("node:path");
-const { json } = require("body-parser");
+const mailchimpData = require(__dirname + "/mailchimpdata.js");
 
 const app = express();
 
@@ -15,13 +14,13 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.post("/", (req, res) => {
-  console.log(req.body);
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-  console.log(firstName + lastName + email);
+  // console.log(req.body);
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  // console.log(firstName + lastName + email);
 
-  var data = {
+  let data = {
     members: [
       {
         email_address: email,
@@ -34,19 +33,18 @@ app.post("/", (req, res) => {
     ],
   };
 
-  var jsondata = JSON.stringify(data);
+  let jsondata = JSON.stringify(data);
 
-  // var apikey = "68e07b689f8144fdc99987c5c5398ca4-us17";
-  var listid = "f95ebea4ab";
+  let listid = mailchimpData.mailchimpListId[0];
 
-  var url = "https://us17.api.mailchimp.com/3.0/" + "lists/" + listid;
+  let url = "https://us17.api.mailchimp.com/3.0/" + "lists/" + listid;
 
-  var options = {
+  let options = {
     method: "POST",
-    auth: "sijia:36157d0c4022ec37110eeb071a5559eb-us17",
+    auth: "sijia:" + mailchimpData.mailchimpKey
   };
 
-  var request = https.request(url, options, (respond) => {
+  let request = https.request(url, options, (respond) => {
     respond.on("data", (data) => {
       const feedback = JSON.parse(data);
       console.log(feedback);
@@ -64,13 +62,11 @@ app.post("/", (req, res) => {
   request.end();
 });
 
-  app.post("/failure", (req, res) => {
-    res.redirect("/");
-  })
+app.post("/failure", (req, res) => {
+  res.redirect("/");
+});
 
-//list id:f95ebea4ab
 // root url:https://<dc>.api.mailchimp.com/3.0/
-//api key: 68e07b689f8144fdc99987c5c5398ca4-us17
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("server is running on port 3000");
