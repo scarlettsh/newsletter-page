2b7b72bad876f88038f71388fd3a23e0fb8a473a
sijiahu <scarlettshcode@gmail.com>
2022-07-14 16:33:59 -0500

    comment dotenv out

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const request = require("request");
 const https = require("node:https");
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = express();
 
